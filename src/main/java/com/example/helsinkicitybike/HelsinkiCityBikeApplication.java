312699c119d92a04fff1a0e09326fd389d46343b
slaivama <slaivama@gmail.com>
2023-01-19 12:57:54 +0200

    initial commit

---
+++ src/main/java/com/example/helsinkicitybike/HelsinkiCityBikeApplication.java
@@ -0,0 +1,13 @@
+package com.example.helsinkicitybike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelsinkiCityBikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelsinkiCityBikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helsinkicitybike/HelsinkiCityBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helsinkicitybike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelsinkiCityBikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
