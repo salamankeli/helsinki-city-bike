754d66b344b7b7e291e75751d4e42bed54c58dc7
slaivama <slaivama@gmail.com>
2023-02-02 19:36:10 +0200

    Integration test for adding a journey

--- src/test/java/com/example/helsinkicitybike/HelsinkiCityBikeApplicationTests.java
+++ src/test/java/com/example/helsinkicitybike/HelsinkiCityBikeApplicationTests.java
@@ -1,13 +1,55 @@
 package com.example.helsinkicitybike;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.helsinkicitybike.Journey.Journey;
+import com.example.helsinkicitybike.Journey.JourneyRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = HelsinkiCityBikeApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application-integrationtest.properties")
 class HelsinkiCityBikeApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    JourneyRepository jRepo;
 
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void testAddingJourney() throws Exception {
+
+        var journey1 = new Journey("2021-05-31T23:57:25","2021-06-01T00:05:46",94,"Laajalahden aukio",100,"Teljäntie",2043,500);
+        jRepo.save(journey1);
+        mvc.perform(get("/journeys/all?page=0&size=10&sortingField=departureStationId&sortingDirection=ASC")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.content[0].departureTime", is("2021-05-31T23:57:25")))
+                .andExpect(jsonPath("$.content[0].returnTime", is("2021-06-01T00:05:46")))
+                .andExpect(jsonPath("$.content[0].departureStationId", is(94)))
+                .andExpect(jsonPath("$.content[0].departureStationName", is("Laajalahden aukio")))
+                .andExpect(jsonPath("$.content[0].returnStationId", is(100)))
+                .andExpect(jsonPath("$.content[0].returnStationName", is("Teljäntie")))
+                .andExpect(jsonPath("$.content[0].distance", is(2043)))
+                .andExpect(jsonPath("$.content[0].duration", is(500)));
+    }
 }
